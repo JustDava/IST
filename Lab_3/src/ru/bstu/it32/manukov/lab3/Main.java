6fa696d16fd33f64f095854b47b9dc95c62e91b0
JustDava <dava.manukov@yandex.ru>
2021-04-18 16:54:33 +0300

    final

---
+++ Lab_3/src/ru/bstu/it32/manukov/lab3/Main.java
@@ -0,0 +1,8 @@
+package ru.bstu.it32.manukov.lab3;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Lab_3/src/ru/bstu/it32/manukov/lab3/Publication.java
@@ -0,0 +1,4 @@
+package ru.bstu.it32.manukov.lab3;
+
+public class Publication {
+}
